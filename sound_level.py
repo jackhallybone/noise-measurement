0dbe799b51af1090315e2bc0d95ff14a1dbf280e
Jack Hallybone <7317987+jackhallybone@users.noreply.github.com>
2025-03-13 16:52:26 +0000

    basic frequency weighting and tests

--- sound_level.py
+++ sound_level.py
@@ -3,7 +3,7 @@
 Based on the publicly available reprint IS 15575 (Part 1 ) : 2005
 https://law.resource.org/pub/in/bis/S04/is.15575.1.2005.pdf
 
-References to sections, figures, etc in the function docs refer to the IS document.
+References to sections, figures, etc in the function docs refer to the IS 15575 document.
 """
 
 import numpy as np
@@ -11,7 +11,7 @@ from scipy import signal
 
 
 def define_time_weighting_filter(fs, weighting):
-    """Define the time-weighting filter in sos format.
+    """Define a time weighting filter in sos format.
 
     Figure 1 describes the filter as "low-pass filter with one real pole at -1/tau".
 
@@ -38,16 +38,18 @@ def define_time_weighting_filter(fs, weighting):
 
 
 def time_weight(fs, weighting, in_data):
-    """Apply time weighting.
+    """Time weight an input signal.
 
     Figure 1 describes the process of time weighting a signal:
         input -> [square] -> [time-weighting filter] -> [sqrt] -> [log] -> output
 
-    Where "time-weighting filter" is realised in `time_weighting_filter()`.
+    Where "time-weighting filter" is realised in `define_time_weighting_filter()`.
 
     Args:
         fs (int): Sampling rate of the system in Hz.
-        weighting (str): Time weighting to apply ("F" for Fast- or "S" for Slow-weighting).
+        weighting (str): Time weighting to apply where the options are:
+            - "F" for Fast weighting, or
+            - "S" for Slow weighting
         in_data (ndarray): Input signal to be time weighted.
 
     Returns:
@@ -59,6 +61,115 @@ def time_weight(fs, weighting, in_data):
     out_data = np.square(in_data)
     out_data = signal.sosfilt(sos, out_data)
     out_data = np.sqrt(out_data)
-    out_data = 20 * np.log10(out_data)
+    out_data = 20 * np.log10(out_data)  # ie, to dBFS
+
+    return out_data
+
+
+def time_weighting_pole_frequencies():
+    """Calculate the pole frequencies from their definitions.
+
+    Sections 5.4.6 to 5.4.11 give the pole frequencies and their definitions.
+
+    Returns:
+        f1 (float): Pole frequency f1 in Hz.
+        f2 (float): Pole frequency f2 in Hz.
+        f3 (float): Pole frequency f3 in Hz.
+        f4 (float): Pole frequency f4 in Hz.
+    """
+
+    # TODO: implement the pole frequency calculations to improve the below approximations
+
+    # Approximate values are given in section 5.4.11
+    f1 = 20.60
+    f2 = 107.7
+    f3 = 737.9
+    f4 = 12194
+
+    return f1, f2, f3, f4
+
+
+# TODO: implement the equation based filter and normalisation process
+
+
+def define_frequency_weighting_filter(fs, weighting):
+    """Define a frequency weighting filter in sos format.
+
+    Section 5.4.6 describes the realisation of the frequency weighting filters:
+        - Z-weighting requires no filter; z(f)=0
+        - C-weighting:
+            - 2 low frequency poles at f1
+            - 2 high frequency poles at f4
+            - 2 zeros at 0Hz
+        - A-weighting extends the C-weighting filter by adding:
+            - 2 coupled first-order high-pass filters with a cut-off at fA
+
+    Where, section 5.4.10 describes the implementaiton of the high-pass filters:
+        - a pole at f2
+        - a pole at f3
+
+    Args:
+        fs (int): Sampling rate of the system in Hz.
+        weighting (str): Frequency weighting to apply where the options are:
+            - "Z" for Z-weighting (ie, no weighting), or
+            - "C" for C-weighting, or
+            - "A" for A-weighting, or
+            - "CtoA" for only the additional filter to take C-weighted to A-weighted
+
+    Returns:
+        sos (ndarray): Array of second-order filter coefficients.
+    """
+
+    f1, f2, f3, f4 = time_weighting_pole_frequencies()
+
+    # https://github.com/endolith/waveform-analysis/blob/master/waveform_analysis/weighting_filters/ABC_weighting.py#L29
+
+    # Z-weighting (no filter) default starting case
+    z = []
+    p = []
+    k = 1
+
+    if weighting == "C" or weighting == "A":
+        # 2x zeros, 2x poles at f1, 2x poles at f4
+        z.extend([0, 0])
+        p.extend([-2 * np.pi * f1, -2 * np.pi * f1, -2 * np.pi * f4, -2 * np.pi * f4])
+
+    if weighting == "A" or weighting == "CtoA":
+        # 1x pole at f2, 1x pole at f3
+        z.extend([0, 0])
+        p.extend([-2 * np.pi * f2, -2 * np.pi * f3])
+
+    # Normalise the filter to 0dB at 1kHz (fr)
+    b, a = signal.zpk2tf(z, p, k)
+    k /= abs(signal.freqs(b, a, [2 * np.pi * 1000])[1][0])
+
+    # Transform into sos representation
+    dz, dp, dk = signal.bilinear_zpk(z, p, k, fs)
+    sos = signal.zpk2sos(dz, dp, dk)
+
+    return sos
+
+
+def frequency_weight(fs, weighting, in_data):
+    """Frequency weight an input signal.
+
+    The frequency weighting filters are realised in `define_frequency_weighting_filter().
+
+    Args:
+        fs (int): Sampling rate of the system in Hz.
+        weighting (str): Frequency weighting to apply where the options are:
+            - "Z" for Z-weighting (ie, no weighting), or
+            - "C" for C-weighting, or
+            - "A" for A-weighting, or
+            - "CtoA" for only the additional filter to take C-weighted to A-weighted
+        in_data (ndarray): Input signal to be time weighted.
+
+    Returns:
+        out_data (ndarray): Output time weighted signal.
+    """
+
+    sos = define_frequency_weighting_filter(fs, weighting)
+
+    out_data = signal.sosfilt(sos, in_data)
 
     return out_data
--- test_sound_level.py
+++ test_sound_level.py
@@ -3,19 +3,28 @@
 Based on the publicly available reprint IS 15575 (Part 1 ) : 2005
 https://law.resource.org/pub/in/bis/S04/is.15575.1.2005.pdf
 
-References to sections, figures, etc in the function docs refer to the IS document.
+References to sections, figures, etc in the function docs refer to the IS 15575 document.
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 from matplotlib.patches import Polygon
+from scipy import fft
 
 import sound_level
 
 # GLOBAL SETTINGS
 fs = 48000
 plot = False
+
+
+if plot:
+
+    @pytest.fixture(scope="session", autouse=True)
+    def show():
+        yield  # run tests first, then show plots all at once
+        plt.show()
 
 
 testdata = [
@@ -62,7 +71,7 @@ def test_time_weighting_decay_rate(weighting, color):
     if plot:
         fig, ax = plt.subplots()
 
-        ax.plot(weighted_signal, color=color)
+        ax.plot(weighted_signal, color=color, label=f"{weighting}-weighted signal")
 
         # Plot the range of permissible decay rates
         top_dB = -3
@@ -79,6 +88,7 @@ def test_time_weighting_decay_rate(weighting, color):
                 fill=True,
                 color=color,
                 alpha=0.1,
+                label=f"Permissible {weighting}-weighting decay range",
             )
         )
 
@@ -86,8 +96,6 @@ def test_time_weighting_decay_rate(weighting, color):
 
         fig.suptitle(f"{weighting}- Time Weighting Decay Rate")
         fig.tight_layout()
-
-        plt.show()
 
 
 def test_time_weighting_steady_state():
@@ -101,8 +109,6 @@ def test_time_weighting_steady_state():
 
     test_signal_1kHz = np.sin(2 * np.pi * 1000 * (np.arange(10 * fs) / fs))  # 10 seconds
 
-    print(test_signal_1kHz.shape)
-
     F_weighted_signal = sound_level.time_weight(fs, "F", test_signal_1kHz)
     S_weighted_signal = sound_level.time_weight(fs, "S", test_signal_1kHz)
 
@@ -112,12 +118,170 @@ def test_time_weighting_steady_state():
     if plot:
         fig, ax = plt.subplots()
 
-        ax.plot(F_weighted_signal, color="tab:blue")
-        ax.plot(S_weighted_signal, color="tab:orange")
+        ax.plot(F_weighted_signal, color="tab:blue", label="F-weighted signal")
+        ax.plot(S_weighted_signal, color="tab:orange", label="S-weighted signal")
 
         ax.set_ylim(-20, 0)
 
         fig.suptitle("Time Weighting Steady State")
         fig.tight_layout()
 
-        plt.show()
+
+testdata = [
+    ("Z", "tab:blue"),
+    ("C", "tab:orange"),
+    ("A", "tab:purple"),
+    ("CtoA", "tab:brown"),
+]
+
+
+@pytest.mark.parametrize("weighting, color", testdata)
+def test_frequency_weighting_and_tolerance(weighting, color):
+    """Validate the frequency weighting is within the tolerance limits.
+
+    The weighting targets and tolerance limits are defined in section 5.4.1 and Table 2.
+    """
+
+    weighting_and_tolerance_limits = np.array(
+        [  # Table 2
+            #     Hz      A-      C-    Z-   Class 1 limits    Class 2 limits
+            [10, -70.4, -14.3, 0.0, +3.5, -np.inf, +5.5, -np.inf],
+            [12.5, -63.4, -11.2, 0.0, +3.0, -np.inf, +5.5, -np.inf],
+            [16, -56.7, -8.5, 0.0, +2.5, -4.5, +5.5, -np.inf],
+            [20, -50.5, -6.2, 0.0, +2.5, -2.5, +3.5, -3.5],
+            [25, -44.7, -4.4, 0.0, +2.5, -2.0, +3.5, -3.5],
+            [31.5, -39.4, -3.0, 0.0, +2.0, -2.0, +3.5, -3.5],
+            [40, -34.6, -2.0, 0.0, +1.5, -1.5, +2.5, -2.5],
+            [50, -30.2, -1.3, 0.0, +1.5, -1.5, +2.5, -2.5],
+            [63, -26.2, -0.8, 0.0, +1.5, -1.5, +2.5, -2.5],
+            [80, -22.5, -0.5, 0.0, +1.5, -1.5, +2.5, -2.5],
+            [100, -19.1, -0.3, 0.0, +1.5, -1.5, +2.0, -2.0],
+            [125, -16.1, -0.2, 0.0, +1.5, -1.5, +2.0, -2.0],
+            [160, -13.4, -0.1, 0.0, +1.5, -1.5, +2.0, -2.0],
+            [200, -10.9, 0.0, 0.0, +1.5, -1.5, +2.0, -2.0],
+            [250, -8.6, 0.0, 0.0, +1.4, -1.4, +1.9, -1.9],
+            [315, -6.6, 0.0, 0.0, +1.4, -1.4, +1.9, -1.9],
+            [400, -4.8, 0.0, 0.0, +1.4, -1.4, +1.9, -1.9],
+            [500, -3.2, 0.0, 0.0, +1.4, -1.4, +1.9, -1.9],
+            [630, -1.9, 0.0, 0.0, +1.4, -1.4, +1.9, -1.9],
+            [800, -0.8, 0.0, 0.0, +1.4, -1.4, +1.9, -1.9],
+            [1000, 0, 0, 0, +1.1, -1.1, +1.4, -1.4],  # 1kHz
+            [1250, +0.6, 0.0, 0.0, +1.4, -1.4, +1.9, -1.9],
+            [1600, +1.0, -0.1, 0.0, +1.6, -1.6, +2.6, -2.6],
+            [2000, +1.2, -0.2, 0.0, +1.6, -1.6, +2.6, -2.6],
+            [2500, +1.3, -0.3, 0.0, +1.6, -1.6, +3.1, -3.1],
+            [3150, +1.2, -0.5, 0.0, +1.6, -1.6, +3.1, -3.1],
+            [4000, +1.0, -0.8, 0.0, +1.6, -1.6, +3.6, -3.6],
+            [5000, +0.5, -1.3, 0.0, +2.1, -2.1, +4.1, -4.1],
+            [6300, -0.1, -2.0, 0.0, +2.1, -2.6, +5.1, -5.1],
+            [8000, -1.1, -3.0, 0.0, +2.1, -3.1, +5.6, -5.6],
+            [10000, -2.5, -4.4, 0.0, +2.6, -3.6, +5.6, -np.inf],
+            [12500, -4.3, -6.2, 0.0, +3.0, -6.0, +6.0, -np.inf],
+            [16000, -6.6, -8.5, 0.0, +3.5, -17.0, +6.0, -np.inf],
+            [20000, -9.3, -11.2, 0.0, +4.0, -np.inf, +6.0, -np.inf],
+        ]
+    )
+
+    weighting_index = 1 if weighting == "A" or weighting == "CtoA" else 2 if weighting == "C" else 3
+    class_1_upper_limits = weighting_and_tolerance_limits[:, weighting_index] + weighting_and_tolerance_limits[:, 4]
+    class_1_lower_limits = weighting_and_tolerance_limits[:, weighting_index] + weighting_and_tolerance_limits[:, 5]
+
+    test_signal_unit_impulse = np.array([1] + [0] * (fs - 1))
+
+    if weighting == "CtoA":
+        weighted_signal = sound_level.frequency_weight(fs, "C", test_signal_unit_impulse)
+        weighted_signal = sound_level.frequency_weight(fs, "CtoA", weighted_signal)
+    else:
+        weighted_signal = sound_level.frequency_weight(fs, weighting, test_signal_unit_impulse)
+
+    # Get the frequency response
+    y = np.abs(fft.rfft(weighted_signal))
+    y = 20 * np.log10(y)  # to dBFS
+    x = np.linspace(0, fs / 2, num=y.shape[0])  # frequency bin numbering
+
+    # Filter the frequency response for only the frequencies defined in Table 2
+    bin_indices = [(np.abs(x - f)).argmin(axis=0) for f in weighting_and_tolerance_limits[:, 0]]
+    y_defined = y[bin_indices]
+
+    assert np.all(y_defined < class_1_upper_limits) and np.all(y_defined > class_1_lower_limits)
+
+    if plot:
+        fig, ax = plt.subplots()
+
+        # Plot target and tolerance limits
+        ax.semilogx(
+            weighting_and_tolerance_limits[:, 0],
+            weighting_and_tolerance_limits[:, weighting_index],
+            linestyle="",
+            marker="x",
+            color="k",
+            label=f"{weighting}-weighting target",
+        )
+        ax.semilogx(
+            weighting_and_tolerance_limits[:, 0],
+            class_1_upper_limits,
+            linestyle="",
+            marker=7,
+            color="k",
+            label=f"Class 1 {weighting}-weighting upper limit",
+        )
+        ax.semilogx(
+            weighting_and_tolerance_limits[:, 0],
+            class_1_lower_limits,
+            linestyle="",
+            marker=6,
+            color="k",
+            label=f"Class 1 {weighting}-weighting lower limits",
+        )
+
+        ax.vlines(fs / 2, ymin=-75, ymax=10, color="k", linestyle=":", label="fs/2")
+
+        ax.semilogx(x, y, color=color, label=f"{weighting}-weighted signal")
+
+        ax.set_xlim(9, (fs / 2) * 1.1)
+        ax.set_ylim(-75, 10)
+        ax.grid()
+        ax.legend(loc="lower center")
+        fig.suptitle(f"{weighting}- Frequency Weighting")
+        fig.tight_layout()
+
+
+def test_frequency_weighting_steady_state():
+    """Validate the frequency weighting steady state response.
+
+    Section 5.4.14 defines the maximum deviation between frequency weighted signals.
+        - shall not exceed +/-0.4dB from the A-weighted
+    """
+
+    test_signal_1kHz = np.sin(2 * np.pi * 1000 * (np.arange(10 * fs) / fs))  # 10 seconds
+
+    Z_weighted_signal = sound_level.frequency_weight(fs, "Z", test_signal_1kHz)
+    C_weighted_signal = sound_level.frequency_weight(fs, "C", test_signal_1kHz)
+    A_weighted_signal = sound_level.frequency_weight(fs, "A", test_signal_1kHz)
+    CtoA_weighted_signal = sound_level.frequency_weight(fs, "CtoA", C_weighted_signal)
+
+    # TODO: using RMS for now, but correct this
+    Z_level = np.sqrt(np.mean(np.square(Z_weighted_signal)))
+    C_level = np.sqrt(np.mean(np.square(C_weighted_signal)))
+    A_level = np.sqrt(np.mean(np.square(A_weighted_signal)))
+    CtoA_level = np.sqrt(np.mean(np.square(CtoA_weighted_signal)))
+
+    assert np.all(np.abs([Z_level, C_level, CtoA_level] - A_level) <= 0.4)
+
+    if plot:
+        fig, ax = plt.subplots()
+
+        ax.plot(Z_weighted_signal, color="tab:blue", label=f"Z-weighted signal (level={Z_level:.4f})")
+        ax.plot(C_weighted_signal, color="tab:orange", label=f"C-weighted signal (level={C_level:.4f})")
+        ax.plot(A_weighted_signal, color="tab:purple", label=f"A-weighted signal (level={A_level:.4f})")
+        ax.plot(
+            CtoA_weighted_signal,
+            color="tab:brown",
+            linestyle="--",
+            label=f"CtoA-weighted signal (level={CtoA_level:.4f})",
+        )
+
+        ax.set_xlim(0, fs / 10)
+        ax.legend(loc="lower center")
+        fig.suptitle("Frequency Weighting Steady State")
+        fig.tight_layout()
